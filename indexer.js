e2f2a57c34cb7876d8b2f3a19eaf1c49f3fbc0bd
Kirill Kosarev <kirr@yandex-team.ru>
2016-04-04 16:38:43 +0300

    js indexer partly working

--- indexer.js
+++ indexer.js
@@ -1,15 +1,19 @@
 //LAT_OFFSET = 0.0035
 //LONG_OFFSET = 0.005
 
-LAT_OFFSET = 0.005
-LONG_OFFSET = 0.0075
+//LAT_OFFSET = 0.005
+//LONG_OFFSET = 0.0075
+
+LAT_OFFSET = 0.02
+LONG_OFFSET = 0.03
 
 // TODO(kirr) max, min
 CITY_TL = [55.916260, 37.320640]
 CITY_BR = [55.566246, 37.914602]
 
-LAT_COUNT = (CITY_TL[0] - CITY_BR[0]) / (2*LAT_OFFSET);
-LONG_COUNT = (CITY_BR[1] - CITY_TL[1]) / (2*LONG_OFFSET);
+LAT_COUNT = Math.floor((CITY_TL[0] - CITY_BR[0]) / (2*LAT_OFFSET));
+LONG_COUNT = Math.floor((CITY_BR[1] - CITY_TL[1]) / (2*LONG_OFFSET));
+QUADS_COUNT = LAT_COUNT * LONG_COUNT;
 
 function QuadCoordsById(id) {
   var i = Math.floor(id / LONG_COUNT);
@@ -29,40 +33,50 @@ var Indexer = function() {
 }
 
 Indexer.prototype.BuildIndex = function(id) {
+  var obj = this;
+  obj.sourceCoords = QuadCoordsById(id);
+  obj.currentProgress = 0;
+  obj.routes = [];
   return new Promise(function(resolve, reject) {
-    this.sourceCoords = QuadCoordsById(id);
-    this.currentProgress = 0;
-    this.resolveCallback = resolve;
-    this.rejectCallback = reject;
-    this.routes = [];
-
-    this.requestRoutes();
+    obj.resolveCallback = resolve;
+    obj.rejectCallback = reject;
+    obj.requestRoutes();
   });
 }
 
 Indexer.prototype.requestRoutes = function() {
   this.currentProgressLimit =
-      Math.min(this.currentProgress + 10, LAT_COUNT * LONG_COUNT);
-  while (this.currentProgress < this.currentProgressLimit) {
-    makeRequest(this.currentProgress, 0);
-    this.currentProgress = this.currentProgress + 1;
+      Math.min(this.currentProgress + 10, QUADS_COUNT);
+  console.log(this.currentProgressLimit);
+  var k = this.currentProgress;
+  while (k < this.currentProgressLimit) {
+    this.makeRequest(k, 0);
+    k = k + 1;
   }
-
-  if (this.currentProgress == LAT_COUNT*LONG_COUNT)
-    this.resolveCallback();
 }
 
 Indexer.prototype.makeRequest = function(targetId, errCount) {
   ymaps.route(
-    [sourceCoords, QuadCoordsById(targetId)],
+    [this.sourceCoords, QuadCoordsById(targetId)],
     {
-      routingMode: routingMode,
+      routingMode: this.routingMode,
       avoidTrafficJams: true,
       multiRoute:true
     }).done(
-        onRequestComplete.bind(this, targetId),
-        onRequestErr.bind(this, targetId, errCount),
+        this.onRequestComplete.bind(this, targetId),
+        this.onRequestErr.bind(this, targetId, errCount),
         this);
+}
+
+Indexer.prototype.onRequestResolved = function(targetId, route) {
+  this.currentProgress = this.currentProgress + 1;
+  if (this.currentProgress == this.currentProgressLimit) {
+    if (this.currentProgressLimit == QUADS_COUNT) {
+      this.resolveCallback();
+    } else {
+      this.requestRoutes();
+    }
+  }
 }
 
 Indexer.prototype.onRequestComplete = function(targetId, route) {
@@ -72,20 +86,15 @@ Indexer.prototype.onRequestComplete = function(targetId, route) {
   if (activeRoute)
     distance = activeRoute.properties.get('duration').value;
   this.routes.push({id:targetId, distance:distance});
-
-  currentProgress = currentProgress + 1;
-  if (currentProgress == currentProgressLimit)
-    requestRoutes(currentProgress);
+  this.onRequestResolved();
 }
 
 Indexer.prototype.onRequestErr = function(targetId, errCount, err) {
   if (errCount > 3) {
-    currentProgress = currentProgress + 1;
-    if (currentProgress == currentProgressLimit)
-      requestRoutes();
+    this.onRequestResolved();
     return;
   }
-  makeRequest(targetId, errCount + 1);
+  this.makeRequest(targetId, errCount + 1);
 }
 
 ymaps.ready(function () {
